84044f07515a30c50f1305fb11e9d1e1d6ce6a67
James Wu <therealjameswu@gmail.com>
2020-01-24 22:15:44 -0500

    Removed unneeded dependencies.

--- catalog/Infrastructure/EntityConfigurations/ModeldydooEntityTypeConfiguration.cs
+++ catalog/Infrastructure/EntityConfigurations/ModeldydooEntityTypeConfiguration.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Pedestal.Catalog.Models;
-using System;
 
 namespace Pedestal.Catalog.Infrastructure.EntityConfigurations
 {
