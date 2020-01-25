74baf5e1c4cf42ba302df3ea4c20d579548bc58f
James Wu <therealjameswu@gmail.com>
2020-01-25 16:27:20 -0500

    Moved PaginatedItemsViewController.cs to lib helper folder.

---
+++ lib/Database/ViewModels/PaginatedItemsViewController.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace Pedestal.Libraries.Database.ViewModels
+{
+    public class PaginatedItemsViewModel<T> where T : class
+    {
+        public int PageIndex { get; private set; }
+
+        public int PageSize { get; private set; }
+
+        public long Count { get; private set; }
+
+        public IEnumerable<T> Data { get; private set; }
+
+        public PaginatedItemsViewModel(int pageIndex, int pageSize, long count, IEnumerable<T> data)
+        {
+            this.PageIndex = pageIndex;
+            this.PageSize = pageSize;
+            this.Count = count;
+            this.Data = data;
+        }
+    }
+}
